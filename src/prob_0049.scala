3cf0a43171b41aac6741baeccc2da89a85f040c1
vieplivee <vieplivee@gmail.com>
2015-03-16 12:59:51 -0400

    51

--- src/prob_0049.scala
+++ src/prob_0049.scala
@@ -7,7 +7,6 @@ import scala.math.abs
 
 def isPrime(n: Int) = (2 to math.sqrt(n).toInt).forall(n%_ != 0)
 def sortDigit(n: Int) = n.toString.toList.sortWith(_ < _).mkString.toInt
-
 def isSingleDistances(v: Set[Int]) = {
   var result = ArrayBuffer(0)
   for (i <- v; j <- v if i < j) {
@@ -16,6 +15,13 @@ def isSingleDistances(v: Set[Int]) = {
   result.groupBy(identity).mapValues(_.size).filter(_._2 > 1).isEmpty
 }
 
-val answer = (1000 to 9999).filter(isPrime).groupBy(sortDigit(_)).
-  map(_._2.toSet).filter(_.size >= 3).flatMap(_.subsets.toList).filter(_.size == 3).
-  filterNot(x => isSingleDistances(x)).tail.map(_.mkString(""))
+val answer = (1000 to 9999).
+  filter(isPrime).
+  groupBy(sortDigit(_)).
+  map(_._2.toSet).
+  filter(_.size >= 3).
+  flatMap(_.subsets.toList).
+  filter(_.size == 3).
+  filterNot(x => isSingleDistances(x)).
+  tail.
+  map(_.mkString(""))
--- src/prob_0051.scala
+++ src/prob_0051.scala
@@ -4,19 +4,15 @@
 
 import scala.math
 
-val minCount = 8
-
 def isPrime(n: Int) = (2 to math.sqrt(n).toInt).forall(n%_ != 0)
 def getPrimeStrings(max: Int) = (1 to max).filter(isPrime).map(_.toString)
-def countPrimes(p: String)(d: Char) = {
-  "0123456789".toList.
-    map(x => p.replace(d, x)).
-    filter(!_.startsWith("0")).
-    map(Integer.parseInt).
-    filter(isPrime).
-    length
-}
-def maxCount(prime: String) = prime.toSet.map(countPrimes(prime)).max
-def answer = getPrimeStrings(10000000).find(maxCount(_) >= minCount).get
-
-println(answer)
+def maxCount(prime: String) = prime.toSet.map{
+	d:Char =>
+		"0123456789".toList.
+	    map(x => prime.replace(d, x)).
+	    filter(!_.startsWith("0")).
+	    map(_.toInt).
+	    filter(isPrime).
+	    length
+}.max
+def answer = getPrimeStrings(Int.MaxValue).find(maxCount(_) >= 8).get
