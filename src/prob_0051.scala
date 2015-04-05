d8216899c911ca092d8698df81023c4ddb5d5457
vieplivee <vieplivee@gmail.com>
2015-04-05 02:33:15 -0400

    add 54

--- src/prob_0051.scala
+++ src/prob_0051.scala
@@ -4,15 +4,16 @@
 
 import scala.math
 
-def isPrime(n: Int) = (2 to math.sqrt(n).toInt).forall(n%_ != 0)
-def getPrimeStrings(max: Int) = (1 to max).filter(isPrime).map(_.toString)
-def maxCount(prime: String) = prime.toSet.map{
-	d:Char =>
-		"0123456789".toList.
-	    map(x => prime.replace(d, x)).
-	    filter(!_.startsWith("0")).
-	    map(_.toInt).
-	    filter(isPrime).
-	    length
-}.max
-def answer = getPrimeStrings(Int.MaxValue).find(maxCount(_) >= 8).get
+def selfPow10Dig(n: Int): Long = {
+	var result = 1L
+	for (x <- (1 to n)) {
+		result = (result * n).toString.takeRight(10).toLong
+	}
+	result
+}
+
+def sum10Dig(m: Long, n: Long) = (m + n).toString.takeRight(10).toLong
+
+def getSum(n: Int) = (1 to n).map(x => selfPow10Dig(x)).reduceLeft((x, y) => sum10Dig(x, y))
+
+val answer = getSum(1000)
